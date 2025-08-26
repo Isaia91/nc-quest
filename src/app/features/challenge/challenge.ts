821eb8e22910581dd9107cb99b4012b4b7d722f4
Isaia91 <isaia.maperi@gmail.com>
2025-08-26 11:00:30 +1100

    creation components

---
+++ src/app/features/challenge/challenge.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-challenge',
+  imports: [],
+  templateUrl: './challenge.html',
+  styleUrl: './challenge.css'
+})
+export class Challenge {
+
+}
